aa8bd09a9c44b5a652f65ff042c812d35674a5c2
Cazeip <45337211+cazeip@users.noreply.github.com>
2022-04-16 21:48:46 +0200

    replaced API with scp

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const port = 4000;
+const path = require("path");
+const { dirname } = require("path");
+const node_scp = require("node-scp");
+const child_process = require("child_process");
 let result;
 
 app.use(express.static('./public'));
@@ -13,7 +17,66 @@ app.listen(port, () => {
 app.get("/data", (req, res) => {
     res.send(result);
 });
+app.get("/reload_cache", (req, res) => {
+    child_process.exec("rm cache/*");
+    node_scp.Client({
+        host: '192.168.1.42',
+        port: 22,
+        username: 'root',
+        password: '2qAgxR8acm'
+    }).then(client => {
+        client.list("/home/root/.local/share/remarkable/xochitl/").then(list => {
+            console.log(list);
+            let toBedownloaded = 0;
+            for(i in list){
+                if(list[i].name.includes(".metadata")){
+                    client.downloadFile(`/home/root/.local/share/remarkable/xochitl/${list[i].name}`, `cache/${list[i].name}`).then(response => {
+                        toBedownloaded--;
+                        if(toBedownloaded == 0){
+                            console.log("Cached Everything");
+                            client.close();
+                            fs.readdir('cache', (err, files) => {
+                                console.log("Found mounting point");
+                                for(i in files){
+                                    if(files[i].includes(".metadata")){
+                                        f = fs.readFileSync(`cache/${files[i]}`, {encoding: 'utf-8'});
+                                        data = JSON.parse(f);
+                                        data.children = {};
 
+                                        currentUUID = files[i].slice(0, files[i].length - ".metadata".length);
+                                        data.selfID = currentUUID;
+                                        filesystem[currentUUID] = data;
+                                    }
+                                }
+                                for(key in filesystem){
+                                    if(key !== "" && key !== "trash") filesystem[filesystem[key].parent].children[key] = filesystem[key];
+                                }
+                                result = filesystem[""];
+                                fs.writeFileSync("cached.json", JSON.stringify(result));
+                                console.log("Successfully indexed file system");
+                                res.status(200).send();
+                            });
+                        }
+                    });
+                    toBedownloaded++;
+                }
+            }
+        });
+    });
+});
+app.get("/pdf/:id", (req, res)=>{
+    console.log(req.params.id);
+    node_scp.Client({
+        host: '192.168.1.42',
+        port: 22,
+        username: 'root',
+        password: '2qAgxR8acm'
+    }).then(client => {
+        client.downloadFile(`/home/root/.local/share/remarkable/xochitl/${req.params.id}.pdf`, `cache/${req.params.id}.pdf`).then(response => {
+            res.sendFile(path.join(__dirname,`cache/${req.params.id}.pdf`));
+        });
+    });
+});
 let filesystem = {};
 filesystem[""] = {
     children: {}
@@ -21,23 +84,4 @@ filesystem[""] = {
 filesystem["trash"] = {
     children: {}
 }
-
-fs.readdir('Remarkable', (err, files) => {
-    console.log("Found mounting point");
-    for(i in files){
-        console.log(files[i]);
-        if(files[i].contains(".metadata")){
-            f = fs.readFileSync(`Remarkable/${files[i]}`, {encoding: 'utf-8'});
-            data = JSON.parse(f);
-            data.children = {};
-
-            currentUUID = files[i].slice(0, files[i].length - ".metadata".length);
-            data.selfID = currentUUID;
-            filesystem[currentUUID] = data;
-        }
-    }
-    for(key in filesystem){
-        if(key !== "" && key !== "trash") filesystem[filesystem[key].parent].children[key] = filesystem[key];
-    }
-    result = filesystem[""];
-});
+result = fs.readFileSync("cached.json", {encoding: "utf-8"});
