97c9ec14031470614ccb3050bb18108cdaebedd0
Cazeip <45337211+cazeip@users.noreply.github.com>
2022-04-16 12:12:32 +0200

    Root

---
+++ index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const express = require("express");
+const app = express();
+const port = 4000;
+let result;
+
+app.use(express.static('./public'));
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`Success! Your application is running on port ${port}.`);
+});
+app.get("/data", (req, res) => {
+    res.send(result);
+});
+
+let filesystem = {};
+filesystem[""] = {
+    children: {}
+}
+filesystem["trash"] = {
+    children: {}
+}
+
+fs.readdir('metadata', (err, files) => {
+    for(i in files){
+        f = fs.readFileSync(`metadata/${files[i]}`, {encoding: 'utf-8'});
+        data = JSON.parse(f);
+        data.children = {};
+
+        currentUUID = files[i].slice(0, files[i].length - ".metadata".length);
+        data.selfID = currentUUID;
+        filesystem[currentUUID] = data;
+    }
+    for(key in filesystem){
+        if(key !== "" && key !== "trash") filesystem[filesystem[key].parent].children[key] = filesystem[key];
+    }
+    result = filesystem[""];
+});
